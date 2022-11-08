5f2d3cadf19956fb19f400815c1fad93627b5307
murilo.eising@escola.pr.gov.br <murilo.eising@escola.pr.gov.br>
2022-11-08 11:49:07 -0300

    ediçao no banco de dados

--- bd.js
+++ bd.js
@@ -3,11 +3,17 @@ var bd = openDatabase("minhaBD", "1.0", "Meu Banco de Dados", 4080);
 bd.transaction(function (criar) {
     criar.executeSql("CREATE TABLE nomes (nome TEXT, idade INTEGER)");
 });
-function salvarNome() {
-    const nomeUsuario = document.getElementById("nome-usuario").value;
+function salvarInfo() {
+    const nomeUsuario = document
+        .getElementById("nome-usuario")
+        .value.toUpperCase();
     const idadeUsuario = parseInt(
         document.getElementById("idade-usuario").value
     );
+    if (nomeUsuario === "" || isNaN(idadeUsuario)) {
+        alert("faltam informações");
+        return false;
+    }
     bd.transaction(function (inserir) {
         inserir.executeSql("INSERT INTO nomes (nome,idade) VALUES (? , ?)", [
             nomeUsuario,
@@ -15,13 +21,45 @@ function salvarNome() {
         ]);
     });
 }
- function pesquisaPorNome(){
-    const nomeUsuario = documente.getElementById("pesquisa-nome-usuario").value;
-    bd.transaction(function (ler){
-ler.executeSql(SELECT * FROM formulario WHERE nome="${nomeUsuario}" , [], function (ler, resultados){
-    const tamanho = resultados.rows.length
-    const msg = tamanho + "linhas encontradas";
+function pesquisaPorNome() {
+    const nomeUsuario = document
+        .getElementById("pesquisa-nome-usuario")
+        .value.toUpperCase();
+    bd.transaction(function (ler) {
+        ler.executeSql(
+            `SELECT * FROM nomes WHERE nome like "%${nomeUsuario}%"`,
+            [],
+            function (ler, resultados) {
+                const tamanho = resultados.rows.length;
+                const msg = tamanho + "linhas encontradas";
+                console.log(msg);
+                const nome = resultados.rows.item(tamanho - 1).nome;
+                const idade = resultados.rows.item(tamanho - 1).idade;
+                document.getElementById("pesquisa-nome-usuario").value = nome;
+                document.getElementById("resultado-pesquisa").value = idade;
+            }
+        );
+    });
 }
-    })
-    alert(nomeUsuario);
- }
+
+function exibeBD() {
+    bd.transaction(function (exibe) {
+        exibe.executeSql(
+            "SELECT * FROM nomes",
+            [],
+            function (exibe, resultados) {
+                const tamanho = resultados.rows.length;
+                let item;
+
+                document.getElementById("lista-bd").innerHTML = "";
+
+                for (let i = 0; i < tamanho; i++) {
+                    item = resultados.rows.item(i);
+                    document.getElementById(
+                        "lista-bd"
+                    ).innerHTML += `<p>Nome: ${item.nome},${item.idade} anos</p>`;
+                }
+            }
+        );
+    });
+}
